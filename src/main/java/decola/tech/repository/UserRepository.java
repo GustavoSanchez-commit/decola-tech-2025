f566ea36d829fbbd1a0d88fd43f198ef8876a166
Gustavo Sanchez <gussan2127@gmail.com>
2025-02-18 14:47:26 -0300

    Create UserRepository

---
+++ src/main/java/decola/tech/repository/UserRepository.java
@@ -0,0 +1,15 @@
+
+package decola.tech.repository;
+
+import decola.tech.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
