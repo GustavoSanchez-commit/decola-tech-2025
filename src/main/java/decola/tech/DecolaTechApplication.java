a2b9826abd54f921199490a22e8d3fb41c2ee5e3
Gustavo Sanchez <gussan2127@gmail.com>
2025-02-18 14:04:03 -0300

    init project
    init project

---
+++ src/main/java/decola/tech/DecolaTechApplication.java
@@ -0,0 +1,13 @@
+package decola.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/decola/tech/DecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package decola.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
